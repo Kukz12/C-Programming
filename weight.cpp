a2a7fb3c87c0c5877a19f3683394e17904375895
Sneha Cenoy <145749059+Kukz12@users.noreply.github.com>
2025-01-17 19:02:25 +0400

    Create weight.cpp

---
+++ weight.cpp
@@ -0,0 +1,10 @@
+int main() {
+  // code
+  double weighte;
+  std::cout << "Enter weight in earth:\n";
+  std::cin >> weighte;
+  double weightm;
+  weightm = weighte * 0.38;
+  std::cout << "The weight in Mars is "<< weightm <<"\n";
+
+}

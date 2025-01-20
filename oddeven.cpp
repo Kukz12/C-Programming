1815165915712fa3197b84b1c54cad149c2300ba
Sneha Cenoy <145749059+Kukz12@users.noreply.github.com>
2025-01-20 20:10:02 +0400

    Create oddeven.cpp

---
+++ oddeven.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+int x;
+std::cout << "ENTER THE NUMBER TO CHECK ODD OR EVEN: ";
+std::cin >> x;
+if(x%2 == 0)
+{
+std::cout << "THE NUMBER IS EVEN ";
+}
+else
+{
+std::cout << "THE NUMBER IS ODD ";
+}
+return 0;
+}
+
+
